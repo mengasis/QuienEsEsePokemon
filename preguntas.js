93c9cc63d288c9d07c14c2eea23f58f754ecbc7e
Diego Hernandez <diego.hernandez@ceinf.cl>
2016-10-05 22:19:18 -0300

    Prototipo pokemon 1

---
+++ preguntas.js
@@ -0,0 +1,42 @@
+[
+  {
+    "numero": 1,
+    "pregunta": "Â¿Que tipo de pokemon es?"
+  },
+  {
+    "numero": 2,
+    "pregunta": "Â¿Tiene Evolucion?"
+  },
+  {
+    "numero": 3,
+    "pregunta": "Â¿Se Parece a un Animal?"
+  },
+  {
+    "numero": 4,
+    "pregunta": "Â¿Como se transporta?"
+  },
+  {
+    "numero": 5,
+    "pregunta": "Â¿habitat?"
+  },
+  {
+    "numero": 6,
+    "pregunta": "Â¿Ha Evolucionado?"
+  },
+  {
+    "numero": 7,
+    "pregunta": "Â¿Tipo de EvoluciÃ³n?"
+  },
+  {
+    "numero": 8,
+    "pregunta": "Â¿Frecuencia?"
+  },
+  {
+    "numero": 9,
+    "pregunta": "Â¿Otro tipo de pokemon?"
+  },
+  {
+    "numero": 10,
+    "pregunta": "Â¿Sexo?"
+  }
+]
